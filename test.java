c6ca177d9123dfb14fa2aba0870a9ea128273cbf
Chaman Gupta <chamangupta31599@gmail.com>
2023-02-19 10:18:46 +0530

    some changes

---
+++ test.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class test
+{
+    public static boolean isValid(String s) {
+        Stack<String> stck=new Stack<String>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                stck.push(String.valueOf(s.charAt(i)));
+                System.out.println(stck);
+            }
+            else {
+                String x = (String) stck.pop();
+                if(x.equals(s.charAt(i)) != false) return false;
+
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String str="{()}";
+        System.out.println(isValid(str)); 
+    }
+}
